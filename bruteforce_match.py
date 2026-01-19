0962111a3b2d488dd387d0e6e27efbd8173a5834
hanltk1106 <hanltk0251@ut.edu.vn>
2026-01-19 13:58:53 +0700

    Update bruteforce_match.py

--- bruteforce_match.py
+++ bruteforce_match.py
@@ -1,15 +1,16 @@
-from segmenter import segment_by_sentence, segment_by_length
+from segmenter import segment_by_sentence
 from preprocess_text import preprocess
 
     #So khớp toàn bộ đoạn 
 
-def brute_force_segment_match(segments1, segments2):
+def brute_force_segment_match(segments1, segments2, threshold=0.6):
     matches = []
 
     for i in range(len(segments1)):
         for j in range(len(segments2)):
-            if segments1[i] == segments2[j]:
-                matches.append((i, j))
+            sim = sentence_similarity(segments1[i], segments2[j])
+            if sim >= threshold:
+                matches.append((i, j,sim))
 
     return matches
 
@@ -27,7 +28,7 @@ def brute_force_word_group(words1, words2, group_size=5):
             if group1 == group2:
                 results.append((i, j, group1))
 
-    return result
+    return results
 
 #Tính % giống nhau
 def calculate_similarity(total_segments, matched_segments):
@@ -55,9 +56,14 @@ if __name__ == "__main__":
     # so khớp đoạn
     matches = brute_force_segment_match(segments1, segments2)
 
-    similarity = calculate_similarity(len(segments1), len(matches))
+    # tổng số đoạn của cả 2 văn bản
+    total_segments = len(segments1) + len(segments2)
+
+    #tính độ tương đồng cho cả text 1 và text 2
+    similarity = calculate_similarity(total_segments, len(matches))*100
 
     print("Số đoạn văn bản 1:", len(segments1))
+    print("Số đoạn văn bản 2:",len(segments2))
     print("Số đoạn trùng:", len(matches))
     print("Phần trăm giống nhau:", similarity, "%")
 
@@ -69,3 +75,9 @@ if __name__ == "__main__":
     word_matches = brute_force_word_group(words1, words2, 5)
 
     print("\nSố nhóm từ trùng:", len(word_matches))
+    print("\n Các nhóm từ trùng nhau:")
+    for i,j, group in word_matches:
+
+        print(f"-Nhóm từ tại vị trí {i} (Text 1) trùng với vị trí {j} (Text 2)")
+
+        print(" Nội dung nhóm từ:","".join(group))
